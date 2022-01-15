119e42729fb7b1ef1e695781012fcd59c8151a0d
sytone <github@sytone.com>
2022-01-14 22:05:47 -0800

    fixed a few errors

--- src/memos.ts
+++ src/memos.ts
@@ -1,55 +1,51 @@
-import{ WorkspaceLeaf , ItemView, HoverPopover } from "obsidian";
-import { MEMOS_VIEW_TYPE } from './constants';
+import { WorkspaceLeaf, ItemView, HoverPopover } from "obsidian";
+import { MEMOS_VIEW_TYPE } from "./constants";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import type MemosPlugin from './index';
+import type MemosPlugin from "./index";
 import { dailyNotesService } from "./services";
 
 export class Memos extends ItemView {
-	plugin: MemosPlugin;
-	hoverPopover: HoverPopover | null;
-	private reactComponent: React.ReactElement;
+  plugin: MemosPlugin;
+  hoverPopover: HoverPopover | null;
+  private reactComponent: React.ReactElement;
 
-	constructor(leaf: WorkspaceLeaf, plugin: MemosPlugin) {
-		super(leaf);
-		this.plugin = plugin;
-	}
+  constructor(leaf: WorkspaceLeaf, plugin: MemosPlugin) {
+    super(leaf);
+    this.plugin = plugin;
+  }
 
-	getDisplayText(): string {
-		// TODO: Make this interactive: Either the active workspace or the local graph
-		return 'Memos';
-	}
+  getDisplayText(): string {
+    // TODO: Make this interactive: Either the active workspace or the local graph
+    return "Memos";
+  }
 
-	getIcon(): string {
-		return "Calendar";
-	}
+  getIcon(): string {
+    return "Calendar";
+  }
 
-	getViewType(): string {
-		return MEMOS_VIEW_TYPE;
-	}
+  getViewType(): string {
+    return MEMOS_VIEW_TYPE;
+  }
 
-	async onOpen(): Promise<void> {
+  async onOpen(): Promise<void> {
+    await this.plugin.loadSettings();
+    dailyNotesService.getApp(this.app);
+    InsertAfter = this.plugin.settings.InsertAfter;
+    UserName = this.plugin.settings.UserName;
+    ProcessEntriesBelow = this.plugin.settings.ProcessEntriesBelow;
 
+    this.reactComponent = React.createElement(App);
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ReactDOM.render(this.reactComponent, (this as any).contentEl);
+  }
 
-		await this.plugin.loadSettings();
-		dailyNotesService.getApp(this.app);
-		InsertAfter = this.plugin.settings.InsertAfter;
-		UserName = this.plugin.settings.UserName;
-		ProcessEntriesBelow = this.plugin.settings.ProcessEntriesBelow;
-
-		this.reactComponent = React.createElement(App);
-
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		ReactDOM.render(this.reactComponent, (this as any).contentEl);
-	}
-
-	async onClose() {
-		// Nothing to clean up.
-	}
-
+  async onClose() {
+    // Nothing to clean up.
+  }
 }
 
 export let InsertAfter: string;
