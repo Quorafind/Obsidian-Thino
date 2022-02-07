988383ad6fae249f84fdea02c1e4ff32e27c09a0
Mara <mara-li@outlook.fr>
2022-02-07 14:58:38 +0100

    feat(fr): Add french translation

--- src/translations/locale/fr.ts
+++ src/translations/locale/fr.ts
@@ -1,3 +1,24 @@
 // français
 
-export default {};
+export const TRANSLATIONS_FR = {
+  welcome: 'Bienvenue dans Mémo !',
+  ribbonIconTitle: 'Mémos',
+  months: [
+    'Janvier',
+    'Février',
+    'Mars',
+    'Avril',
+    'Mai',
+    'Juin',
+    'Juillet',
+    'Aout',
+    'Septembre',
+    'Octobre',
+    'Novembre',
+    'Décembre',
+  ],
+  weekDays: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'],
+  weekDaysShort: ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim'],
+  to: 'à',
+};
+
