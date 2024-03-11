a15064a940acc79b52587beb563d2b8c0d9f8bc4
quorafind <951011105@qq.com>
2024-03-11 23:23:04 +0800

    chore: bump version

--- .release-it.cjs
+++ .release-it.cjs
@@ -1,41 +1,42 @@
 module.exports = {
-  hooks: {
-    "before:init": ["cd .. && pnpm run build"],
-    "after:bump": [
-      "node ./scripts/zip.mjs",
-      "git add .",
-    ],
-    "after:release":
-      "echo Successfully released obsidian plugin ${name} v${version} to ${repo.repository}.",
-  },
-  git: {
-    commitMessage: "chore: release obsidian plugin v${version}",
-    tagName: "${version}",
-    tagAnnotation: "Release Obsidian Plugin v${version}",
-    addUntrackedFiles: true,
-  },
-  plugins: {
-    // "@release-it/conventional-changelog": {
-    //   preset: "angular",
-    //   infile: "CHANGELOG.md",
-    // },
-    "./scripts/ob-bumper.mjs": {
-      indent: 2,
-      copyTo: ".",
+    hooks: {
+        "before:init": ["cd .. && pnpm run build"],
+        "after:bump": [
+            "node ./scripts/zip.mjs",
+            "git add .",
+        ],
+        "after:release":
+            "echo Successfully released obsidian plugin ${name} v${version} to ${repo.repository}.",
     },
-  },
-  npm: {
-    publish: false,
-  },
-  github: {
-    release: true,
-    assets: [
-      "dist/main.js",
-      "dist/manifest.json",
-      "dist/styles.css",
-      "dist/thino-${version}.zip",
-    ],
-    proxy: process.env.HTTPS_PROXY,
-    releaseName: "${version}",
-  },
+    git: {
+        pushArgs: "--follow-tags -o ci.skip",
+        commitMessage: "chore: release obsidian plugin v${version}",
+        tagName: "${version}",
+        tagAnnotation: "Release Obsidian Plugin v${version}",
+        addUntrackedFiles: true,
+    },
+    plugins: {
+        // "@release-it/conventional-changelog": {
+        //   preset: "angular",
+        //   infile: "CHANGELOG.md",
+        // },
+        "./scripts/ob-bumper.mjs": {
+            indent: 2,
+            copyTo: "./dist",
+        },
+    },
+    npm: {
+        publish: false,
+    },
+    github: {
+        release: true,
+        assets: [
+            "dist/main.js",
+            "dist/manifest.json",
+            "dist/styles.css",
+            "dist/obsidian-thino-${version}.zip",
+        ],
+        proxy: process.env.HTTPS_PROXY,
+        releaseName: "${version}",
+    },
 };
